8998bb53d519e54125b3812e8184dc4d0dd1c0aa
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-01-25 21:44:08 -0500

    finish turn computer and player

--- js/game.js
+++ js/game.js
@@ -4,10 +4,22 @@
 // 2S = Two of Spades
 
 let deck = [];
-let types = ["C", "D", "H", "S"];
-let special = ["J", "Q", "K", "A"];
+const types = ["C", "D", "H", "S"];
+const special = ["J", "Q", "K", "A"];
 
-//This function allows you to create a new deck
+let pointsPlayer = 0;
+let pointsComputer = 0;
+
+//* Refences HTML
+const btnNew = document.querySelector("#btnNew");
+const btnTake = document.querySelector("#btnTake");
+const btnStop = document.querySelector("#btnStop");
+let pointsHtml = document.querySelectorAll("small");
+
+const divCardPlayer = document.querySelector("#player-cards");
+const divCardComputer = document.querySelector("#computer-cards");
+
+//*?This function allows you to create a new deck
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let type of types) {
@@ -23,7 +35,7 @@ const createDeck = () => {
 
   //   console.log(deck);
   deck = _.shuffle(deck);
-  console.log(deck);
+
   return deck;
 };
 createDeck();
@@ -37,17 +49,96 @@ const takeCard = () => {
 
   const card = deck.pop();
 
-  console.log(deck);
-  console.log(card);
   return card;
 };
 
 // takeCard();
+//? Assigning value to cards
 const valueCard = (card) => {
   const value = card.substring(0, card.length - 1);
   return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
 };
 
-const value = valueCard( takeCard() );
-console.log({value}); 
-console.log(value);
+//* Events
+//? Computer's Turn
+const computerTurn = (minimunPoints) => {
+  do {
+    const card = takeCard();
+
+    pointsComputer = pointsComputer + valueCard(card);
+    pointsHtml[1].innerHTML = pointsComputer;
+
+    const imgCard = document.createElement("img");
+    imgCard.src = `/assets/cartas/${card}.png`;
+    imgCard.classList.add("cardPlayer");
+    divCardComputer.append(imgCard);
+
+    if (minimunPoints > 21) {
+      break;
+    }
+  } while (pointsComputer < minimunPoints && minimunPoints <= 21);
+
+  setTimeout(() => {
+    if (pointsComputer === minimunPoints) {
+      alert("Nobody wins");
+    } else if (minimunPoints > 21) {
+      alert("Computer Wins");
+    } else if (pointsComputer > 21) {
+      alert("Player Wins");
+    } else {
+      alert("Computer Wins")
+    }
+  }, 100);
+};
+
+//? Player's Turn
+btnTake.addEventListener("click", () => {
+  const card = takeCard();
+
+  pointsPlayer = pointsPlayer + valueCard(card);
+  pointsHtml[0].innerHTML = pointsPlayer;
+
+  const imgCard = document.createElement("img");
+  imgCard.src = `/assets/cartas/${card}.png`;
+  imgCard.classList.add("cardPlayer");
+  divCardPlayer.append(imgCard);
+
+  // await new Promise((resolve) => setTimeout(resolve, 100));
+
+  if (pointsPlayer > 21) {
+    console.warn('I"m sorry, you lost');
+    btnTake.disabled = true;
+    btnStop.disabled = true;
+    computerTurn(pointsPlayer);
+  } else if (pointsPlayer === 21) {
+    console.warn("21, cool!");
+    btnTake.disabled = true;
+    btnStop.disabled = true;
+    computerTurn(pointsPlayer);
+  }
+});
+
+btnStop.addEventListener("click", () => {
+  btnTake.disabled = true;
+  btnStop.disabled = true;
+  computerTurn(pointsPlayer);
+});
+
+btnNew.addEventListener("click", () => {
+  console.clear();
+  deck = []
+  deck = createDeck();
+
+  pointsPlayer = 0;
+  pointsComputer = 0;
+
+  pointsHtml[0].innerText = 0;
+  pointsHtml[1].innerText = 0;
+
+  divCardComputer.innerHTML = '';
+  divCardPlayer.innerHTML = '';
+
+  btnTake.disabled = false;
+  btnStop.disabled = false;
+
+})
